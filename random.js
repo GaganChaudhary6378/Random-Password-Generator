0f46d4b80bd58bd10b47de591237ef9b976ca72a
Gagan Chaudhary <singhgaganbtp@gmail.com>
2022-09-05 13:20:47 +0530

    generate passwords

---
+++ random.js
@@ -0,0 +1,24 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+
+let passEL1=document.getElementById("pass-1");
+let passEL2=document.getElementById("pass-2");
+
+let isGenerated=false;
+
+function generate(){
+    if(isGenerated===false){
+        for(let i=0;i<=15;i++){
+            passEL1.textContent+=characters[getRandom()];
+        }
+        for(let i=0;i<=15;i++){
+            passEL2.textContent+=characters[getRandom()];
+        }isGenerated=true;
+    }
+}
+
+function getRandom(){
+    let randomNum=Math.floor(Math.random()*characters.length);
+    return randomNum;
+}

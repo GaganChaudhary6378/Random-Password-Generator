4921ee5d822d0ecfef141697030578ae10620a02
Gagan Chaudhary <singhgaganbtp@gmail.com>
2022-09-15 14:15:56 +0530

    keyboard working

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let passEL2=document.getElementById("pass-2");
 let isGenerated=false;
 let string="";
 let count=0;
+let count1=0;
 
 function adder(e){
     if(count<15){
@@ -18,15 +19,24 @@ function adder(e){
 console.log(arr)
 
 function generate1(){
-    if(isGenerated===false){
-        for(let i=0;i<=15;i++){
+    if(isGenerated===false && arr.length===0){
+            passEL1.textContent="";
+            passEL2.textContent="";
+            alert("Please enter letters from virtual keyboard")
+    }else if(count1===0 && isGenerated===false){
+        for(let i=0;i<15;i++){
             passEL1.textContent+=arr[getRandom()];
         }
-        for(let i=0;i<=15;i++){
+        for(let i=0;i<15;i++){
             passEL2.textContent+=arr[getRandom()];
-        }isGenerated=true;
+        }
+        isGenerated=true;
+        count1++;
     }
+    getText.textContent="";
+    arr=[];
 }
+
 
 function getRandom(){
     let randomNum=Math.floor(Math.random()*arr.length);
